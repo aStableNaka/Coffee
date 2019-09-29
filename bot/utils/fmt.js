26e747bb6ec216ea2e487a14db08859391a75716
aStableNaka <trandap88@gmail.com>
2019-09-28 23:58:45 -0400

    bug fixes

--- bot/utils/fmt.js
+++ bot/utils/fmt.js
@@ -348,7 +348,8 @@ function inventory( inventoryObject, entriesPerPage=20, page=0, filter=()=>{retu
 		// Add back for rank
 		//  (${new Array( itemObject.rank+1 ).fill('⭐').join("")})
 		let output = `${ ufmt.item(itemData, itemData.amount, '**') } *${(itemUtils.rankNames[ itemObject.getUniqueRank( itemData ) ]||"Unranked").toLowerCase()}*`;
-		if(itemData.accessor="pickaxe"){
+		if(itemData.accessor=="pickaxe"){
+			console.log(itemData);
 			if(itemData.meta.perks[0]){
 				output+="\n"+(itemData.meta.perks.map((x)=>{return `> + ${x}`}).join("\n"));
 			}
