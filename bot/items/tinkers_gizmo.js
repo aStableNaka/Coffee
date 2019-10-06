4b8730ac426b1ce1d047a9a5d24ce0480ebf1711
aStableNaka <trandap88@gmail.com>
2019-10-06 06:07:28 -0400

    -

--- bot/items/tinkers_gizmo.js
+++ bot/items/tinkers_gizmo.js
@@ -3,27 +3,27 @@ const ufmt = require("../utils/fmt.js");
 const itemUtils = require("../utils/item.js");
 const locale = require("../data/EN_US");
 
-class ItemTinkersGizmo extends Item{
-	constructor(){
-		super( false );
+class ItemTinkersGizmo extends Item {
+	constructor() {
+		super(false);
 		this.name = "Tinkers Gizmo"; // Required
 		this.accessor = "tinkers_gizmo"; // Virtural
 		this.consumable = true;
 		this.value = 0;
-		this.rank = 4;
+		this.rank = 6;
 		this.canUseMulti = true;
 		this.effect = "Increases amount of perk slots for your active pickaxe.";
 		this.icon = "https://i.imgur.com/5jC8IIi.png";
 		this.isDroppedByLootbox = true;
 	}
-	
-	use( Chicken, itemData ){
-		itemUtils.items.pickaxe.ensureUserHasDefaultPickaxe( Chicken.userData );
+
+	use(Chicken, itemData) {
+		itemUtils.items.pickaxe.ensureUserHasDefaultPickaxe(Chicken.userData);
 		let pickaxeItemData = Chicken.userData.items[Chicken.userData.pickaxe_accessor];
-		let slots = itemUtils.items.pickaxe.getMaxPerkSlots( pickaxeItemData );
-		let amount = Chicken.mArgs.amount||1;
-		pickaxeItemData.meta.maxPerkSlots+=amount;
-		Chicken.userData.pickaxe_maxPerkSlots+=amount; // TODO depreciate pickaxe_*
+		let slots = itemUtils.items.pickaxe.getMaxPerkSlots(pickaxeItemData);
+		let amount = Chicken.mArgs.amount || 1;
+		pickaxeItemData.meta.maxPerkSlots += amount;
+		Chicken.userData.pickaxe_maxPerkSlots += amount; // TODO depreciate pickaxe_*
 
 		Chicken.send(ufmt.itemUsedResponse(
 			`You use ${ufmt.plural(amount, 'a', 'some')} ${ufmt.block(`Tinkers ${ufmt.plural(amount, 'Gizmo', 'Gizmos')}!`)}`,
@@ -34,7 +34,7 @@ class ItemTinkersGizmo extends Item{
 		));
 	}
 
-	desc( Chicken, itemData ){
+	desc(Chicken, itemData) {
 		return ufmt.itemDesc([
 			"*Not enough perk slots? Not a problem!*",
 			ufmt.denote('Type', ufmt.block('Pickaxe Modification')),
