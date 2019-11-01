274aae0239115b2d225324495185601ee043f91e
aStableNaka <trandap88@gmail.com>
2019-11-01 15:57:35 -0400

    I FEEL ALII-II-II-IIIVE!

--- bot/commands/market.js
+++ bot/commands/market.js
@@ -72,8 +72,7 @@ class CommandMarket extends Command {
 		}else if( args[0] == 'catalogue'){
 			mArgs.itemAccessor = Chicken.keyPairs.item || (args.slice(1).join(' ').match(/([^!@\d\s])[\d?\w?\.?]+/gi)||[]).join("_").toLowerCase() || false;
 		}else if( args[0] == 'buy'){
-			console.log(args);
-			mArgs.marketCode = escape(Chicken.keyPairs.code) || escape(args[1].toLowerCase()) || false;
+			mArgs.marketCode = escape(Chicken.keyPairs.code || args[1].toLowerCase()) || null;
 		}
 		return mArgs;
 	}
