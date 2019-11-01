392d044239624883a3d64b44e14152aece47e904
aStableNaka <trandap88@gmail.com>
2019-11-01 16:02:44 -0400

    minor fixes

--- bot/views/market/buy.js
+++ bot/views/market/buy.js
@@ -3,9 +3,9 @@ const ufmt = require("../../utils/fmt.js");
 module.exports = function( Chicken, marketEntry, silverItemData ){
 	return ufmt.join( [
 		`${ufmt.block(Chicken.userData.name)}`,
-		`> - ${ufmt.item(silverItemData)}`,
+		`> - ${ufmt.item(silverItemData, marketEntry.price)}`,
 		`> + ${ufmt.item(marketEntry.itemData)}`,
-		`${ufmt.block(marketEntry.name)}`,
+		`${ufmt.block(marketEntry.ownerName)}`,
 		`> + ${ufmt.item(silverItemData)}`
 	] )
 }
